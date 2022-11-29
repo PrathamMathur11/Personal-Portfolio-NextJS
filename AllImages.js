364b7810ca30772cefce94df80be7d568ea30d4d
PrathamMathur11 <mathurpratham010205@gmail.com>
2022-11-29 08:06:06 +0530

    first commit

---
+++ AllImages.js
@@ -0,0 +1,41 @@
+import teerexStore from './public/assets/teerexStore.png'
+import vPlay from './public/assets/vPlay.png'
+import luxorTrade from './public/assets/luxorTrade.png'
+import disneyPlus from './public/assets/disneyPlus.png'
+import bookMyShow from './public/assets/bookMyShow.png'
+import notifyTask from './public/assets/notifyTask.png'
+
+const allProjectsData = [
+  {
+    title: 'Teerex Store Ecommerce Website',
+    imageURL: teerexStore,
+    redirectLink: 'https://teerex-store-ecom.netlify.app/'
+  },
+  {
+    title: 'vPlay Video Player App',
+    imageURL: vPlay,
+    redirectLink: 'https://v-play-stream.netlify.app/'
+  },
+  {
+    title: 'Luxor Trade Ecommerce Website',
+    imageURL: luxorTrade,
+    redirectLink: ''
+  },
+  {
+    title: 'Disney+ Clone Website',
+    imageURL: disneyPlus,
+    redirectLink: 'https://disney-plus-clone-1ef6b.web.app/'
+  },
+  {
+    title: 'BookMyShow UI Clone Website',
+    imageURL: bookMyShow,
+    redirectLink: 'https://bookmyshowapp.vercel.app/'
+  },
+  {
+    title: 'Notify Task Website',
+    imageURL: notifyTask,
+    redirectLink: ''
+  }
+]
+
+export default allProjectsData
