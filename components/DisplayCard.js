6bd77f456d6971cd62ceef3d71b2cabb12d601bf
PrathamMathur11 <mathurpratham010205@gmail.com>
2022-12-31 00:24:26 +0530

    updated sections

---
+++ components/DisplayCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const DisplayCard = () => {
+  return (
+    <div class="container-1 text-teal-500 dark:text-white">
+      <p>
+        Coding is <span class="typed-text"></span>
+        <span class="cursor">&nbsp;</span>
+      </p>
+    </div>
+  );
+};
+
+export default DisplayCard;
