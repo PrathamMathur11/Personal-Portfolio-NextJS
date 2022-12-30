ff7dd926b84a09e5bbe0ac636bfc40324bc01988
PrathamMathur11 <mathurpratham010205@gmail.com>
2022-12-31 01:11:33 +0530

    display card tags

--- components/DisplayCard.js
+++ components/DisplayCard.js
@@ -3,10 +3,12 @@ import React from "react";
 const DisplayCard = () => {
   return (
     <div className="container-1 text-teal-500 dark:text-white new-font">
+      <span className="text-teal-500 new-font">&lt;&gt;</span>
       <p>
         Coding is <span className="typed-text"></span>
         <span className="cursor">&nbsp;</span>
       </p>
+      <span className="text-teal-500 new-font">&lt;/&gt;</span>
     </div>
   );
 };
