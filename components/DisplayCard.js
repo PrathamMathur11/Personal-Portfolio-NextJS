9d1e25985327d27fca8f1e401602d8c614f3a20f
PrathamMathur11 <mathurpratham010205@gmail.com>
2022-12-31 01:08:26 +0530

    font

--- components/DisplayCard.js
+++ components/DisplayCard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const DisplayCard = () => {
   return (
-    <div className="container-1 text-teal-500 dark:text-white">
+    <div className="container-1 text-teal-500 dark:text-white new-font">
       <p>
         Coding is <span className="typed-text"></span>
         <span className="cursor">&nbsp;</span>
