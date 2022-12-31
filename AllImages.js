87efb4c5557283ddd0da0af85557c89c53041584
PrathamMathur11 <mathurpratham010205@gmail.com>
2022-12-31 13:46:50 +0530

    updated redirect links

--- AllImages.js
+++ AllImages.js
@@ -1,41 +1,59 @@
-import teerexStore from './public/assets/teerexStore.png'
-import vPlay from './public/assets/vPlay.png'
-import luxorTrade from './public/assets/luxorTrade.png'
-import disneyPlus from './public/assets/disneyPlus.png'
-import bookMyShow from './public/assets/bookMyShow.png'
-import notifyTask from './public/assets/notifyTask.png'
+import teerexStore from "./public/assets/teerexStore.png";
+import vPlay from "./public/assets/vPlay.png";
+import luxorTrade from "./public/assets/luxorTrade.png";
+import disneyPlus from "./public/assets/disneyPlus.png";
+import bookMyShow from "./public/assets/bookMyShow.png";
+import notifyTask from "./public/assets/notifyTask.png";
+import BezuReWeb from "./public/assets/Bezu-re-web.png";
+import youtubeUiCloneMui from "./public/assets/youtube-ui-clone-mui.png";
+import AccountingCompanyPortfolio from "./public/assets/AccountingCompanyPortfolio.png";
 
 const allProjectsData = [
   {
-    title: 'Teerex Store Ecommerce Website',
+    title: "Teerex Store Ecommerce Website",
     imageURL: teerexStore,
-    redirectLink: 'https://teerex-store-ecom.netlify.app/'
+    redirectLink: "https://teerex-store-ecom.netlify.app",
   },
   {
-    title: 'vPlay Video Player App',
+    title: "Accounting Company Portfolio using HTML & CSS",
+    imageURL: AccountingCompanyPortfolio,
+    redirectLink: "https://accounting-company-portfolio.netlify.app",
+  },
+  {
+    title: "Bezu-re-web",
+    imageURL: BezuReWeb,
+    redirectLink: "https://bezu-re-web.netlify.app",
+  },
+  {
+    title: "youtube UI Clone using Material UI",
+    imageURL: youtubeUiCloneMui,
+    redirectLink: "https://youtube-ui-clone-mui.netlify.app",
+  },
+  {
+    title: "vPlay Video Player App",
     imageURL: vPlay,
-    redirectLink: 'https://v-play-stream.netlify.app/'
+    redirectLink: "https://v-play-stream.netlify.app",
   },
   {
-    title: 'Luxor Trade Ecommerce Website',
+    title: "Luxor Trade Ecommerce Website",
     imageURL: luxorTrade,
-    redirectLink: ''
+    redirectLink: "",
   },
   {
-    title: 'Disney+ Clone Website',
+    title: "Disney+ Clone Website",
     imageURL: disneyPlus,
-    redirectLink: 'https://disney-plus-clone-1ef6b.web.app/'
+    redirectLink: "https://disney-plus-clone-1ef6b.web.app",
   },
   {
-    title: 'BookMyShow UI Clone Website',
+    title: "BookMyShow UI Clone Website",
     imageURL: bookMyShow,
-    redirectLink: 'https://bookmyshowapp.vercel.app/'
+    redirectLink: "https://bookmyshowapp.vercel.app",
   },
   {
-    title: 'Notify Task Website',
+    title: "Notify Task Website",
     imageURL: notifyTask,
-    redirectLink: ''
-  }
-]
+    redirectLink: "https://prathammathur11.github.io/NOTIFY_Task",
+  },
+];
 
-export default allProjectsData
+export default allProjectsData;
