211312e27f209e64e9f86c89c76cb9f5b1929db5
PrathamMathur11 <mathurpratham010205@gmail.com>
2023-01-27 17:25:05 +0530

    .

--- pages/index.js
+++ pages/index.js
@@ -87,7 +87,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900 z-0">
+      <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900 z-10">
         <header className="sticky top-0 py-10 mb-1 flex justify-between dark:text-white">
           <h1 className="text-xl font-burtons">Developed by Pratham Mathur</h1>
           <div className="navbar-links bg-white dark:bg-gray-900">
