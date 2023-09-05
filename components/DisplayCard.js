7c98d9292b7b1f96cb98bdf68cbb01258bb72f6e
PrathamMathur11 <mathurpratham010205@gmail.com>
2023-09-05 15:10:52 +0530

    Updated Typwriter

--- components/DisplayCard.js
+++ components/DisplayCard.js
@@ -1,12 +1,28 @@
 import React from "react";
+import Typed from 'typed.js';
 
 const DisplayCard = () => {
+
+  const el = React.useRef(null);
+
+  React.useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["HARD", "FUN", "A JOURNEY", "LIFE"],
+      typeSpeed: 200,
+      loop: true
+    });
+
+    return () => {
+      typed.destroy();
+    }
+  }, [])
+
+
   return (
     <div className="container-1 text-teal-500 dark:text-white new-font">
       <span className="text-teal-500 new-font">&lt;&gt;</span>
       <p>
-        Coding is <span className="typed-text"></span>
-        <span className="cursor">&nbsp;</span>
+        Coding is <span ref={el} className="typed-text"></span>
       </p>
       <span className="text-teal-500 new-font">&lt;/&gt;</span>
     </div>
